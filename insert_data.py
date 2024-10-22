b83076aeb5b5ff89572013b9623c16ec02ec2906
anuarmusa <anuarmusa@tmrnd.com.my>
2024-10-22 08:56:44 +0800

    "Fix insert_data and update the dependencies"

--- insert_data.py
+++ insert_data.py
@@ -1,26 +1,36 @@
 from dotenv import find_dotenv, load_dotenv
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.pgvector import PGVector
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.document_loaders import DirectoryLoader,TextLoader
+from langchain_postgres import PGVector
+from langchain_postgres.vectorstores import PGVector
+from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-load_dotenv(find_dotenv())
 
-embeddings = OpenAIEmbeddings()
-loader = DirectoryLoader(
-    "./FAQ", glob="**/*.txt", loader_cls=TextLoader, show_progress=True
-)
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+# load txt document to the vector DB
+# loader = DirectoryLoader(
+#     "./FAQ", glob="**/*.txt", loader_cls=TextLoader, show_progress=True
+# )
+# documents = loader.load()
+# text_splitter=RecursiveCharacterTextSplitter(chunk_size=250,chunk_overlap=200)
+# docs=text_splitter.split_documents(documents)
+
+# load pdf to the vector DB
+from langchain_community.document_loaders import PyPDFLoader
+loader=PyPDFLoader('Doc/attention.pdf')
+docs=loader.load()
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+documents=text_splitter.split_documents(docs)
 
 # PGVector needs the connection string to the database.
 CONNECTION_STRING = "postgresql+psycopg://langchain:langchain@localhost:5433/langchain"
-COLLECTION_NAME="restaurant"
+COLLECTION_NAME="vectordb"
 
-PGVector.from_documents(
+# store the doc in the vector DB
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+store = PGVector.from_documents(
     docs,
-    embeddings,
+    embedding=embeddings,
     collection_name=COLLECTION_NAME,
-    connection_string=CONNECTION_STRING
+    connection=CONNECTION_STRING,
+    use_jsonb=True,
 )
